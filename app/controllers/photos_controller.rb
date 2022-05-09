c70dcabcfec5fa20d0b1d0d5422b6f70c9f2b3f1
kimielska <katie.imielska@chicagobooth.edu>
2022-05-09 17:33:32 +0000

    done with the read part of crud (created comment form template)

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,4 +8,13 @@ class PhotosController <ApplicationController
     render({:template => "photo_templates/index.html.erb"})
   end
 
+  def show
+    #parameters: {"path_photo_id"=>"777"}
+    url_id = params.fetch("path_photo_id")
+    matching_photos = Photo.where({:id => url_id})
+    @the_photo =matching_photos.at(0)
+    
+    render({:template => "photo_templates/show.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
   get("/", {:controller => "users", :action => "index"})
-  
+
   get("/users", {:controller => "users", :action => "index"})
 
 get("/users/:id_username", {:controller => "users", :action => "show"})
 
 get("/photos", {:controller => "photos", :action =>"index"})
+
+get("/photos/:path_photo_id", {:controller => "photos", :action =>"show"})
 
 end
