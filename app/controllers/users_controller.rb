665722b23ae15b2555713ea6fc1a87f7f303d926
kimielska <katie.imielska@chicagobooth.edu>
2022-05-09 16:58:56 +0000

    show details form template created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
 
     render({:template => "user_templates/index.html.erb"})
   end
+
+  def show
+    #parameters: {"id_username"=>"anisa"}
+
+    url_username = params.fetch("id_username")
+
+    matching_usernames = User.where({:username => url_username})
+
+    @the_user = matching_usernames.first
+
+    #if the above returns nil, use the defensive approach outlined below
+    # if the_user == nil 
+    #   redirect_to("/404")
+    # else
+      render({:template => "user_templates/show.html.erb"})
+    # end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
 get("/users", {:controller => "users", :action => "index"})
 
+get("/users/:id_username", {:controller => "users", :action => "show"})
 
 end
