52e2940671c4b3274a1e4e1255131187b79c87f9
kimielska <katie.imielska@chicagobooth.edu>
2022-05-09 17:45:01 +0000

    delete complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,20 +1,32 @@
-class PhotosController <ApplicationController
+class PhotosController < ApplicationController
   def index
-    
     matching_photos = Photo.all
-    
-    @list_of_photos = matching_photos.order({:created_at => :desc})
 
-    render({:template => "photo_templates/index.html.erb"})
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+
+    render({ :template => "photo_templates/index.html.erb" })
   end
 
   def show
     #parameters: {"path_photo_id"=>"777"}
     url_id = params.fetch("path_photo_id")
-    matching_photos = Photo.where({:id => url_id})
-    @the_photo =matching_photos.at(0)
-    
-    render({:template => "photo_templates/show.html.erb"})
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show.html.erb" })
   end
 
+  def bye
+    the_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => the_id })
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    #render({ :template => "photo_templates/bye.html.erb" })
+
+    redirect_to("/photos")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ get("/photos", {:controller => "photos", :action =>"index"})
 
 get("/photos/:path_photo_id", {:controller => "photos", :action =>"show"})
 
+get("delete_photo/:path_id",{:controller => "photos",:action => "bye" })
+
 end
