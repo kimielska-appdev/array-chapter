d4001611b05fdaae3f082a675c51a88d31643c49
kimielska <katie.imielska@chicagobooth.edu>
2022-05-09 17:21:59 +0000

    photo index looks good

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-get("/users", {:controller => "users", :action => "index"})
+  get("/", {:controller => "users", :action => "index"})
+  
+  get("/users", {:controller => "users", :action => "index"})
 
 get("/users/:id_username", {:controller => "users", :action => "show"})
+
+get("/photos", {:controller => "photos", :action =>"index"})
 
 end
